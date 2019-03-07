53c37c5b15df5a9bd45169d64d7547b045e000c8
sevastian25 <sevastiancristi@gmail.com>
2019-03-07 11:16:24 +0000

    Add files via upload

---
+++ 1to10Display/1to10Display/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _1to10Display
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Do Loop");
+            while(i != 11)
+            {
+                Console.WriteLine(i++);
+                Thread.Sleep(200);
+            }
+
+            int j = 1;
+            Console.WriteLine("Do While Loop");
+            do
+            {
+                Console.WriteLine(j++);
+                Thread.Sleep(200);
+            }while (j != 11) ;
+        }
+    }
+}
